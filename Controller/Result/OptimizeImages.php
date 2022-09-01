6bbb7cebe16b53b6a592913bfa0ff4103ba2f69c
Vladymyr Hrivinskyi <volodymyr@hryvinskyi.com>
2022-09-01 10:43:02 +0300

    Update OptimizeImages.php

--- Controller/Result/OptimizeImages.php
+++ Controller/Result/OptimizeImages.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace Hryvinskyi\SeoImageOptimizerFrontendUi\Controller\Result;
 
+use Hryvinskyi\SeoImageOptimizerApi\Model\ConfigInterface;
 use Hryvinskyi\SeoImageOptimizerApi\Model\ImageParserInterface;
 use Magento\Framework\App\Response\Http;
 use Magento\Framework\App\ResponseInterface;
@@ -16,13 +17,17 @@ use Magento\Framework\View\Result\Layout;
 class OptimizeImages
 {
     private ImageParserInterface $convertor;
+    private ConfigInterface $config;
 
     /**
      * @param ImageParserInterface $convertor
      */
-    public function __construct(ImageParserInterface $convertor)
-    {
+    public function __construct(
+        ImageParserInterface $convertor,
+        ConfigInterface $config
+    ) {
         $this->convertor = $convertor;
+        $this->config = $config;
     }
 
     /**
@@ -34,6 +39,10 @@ class OptimizeImages
      */
     public function afterRenderResult(Layout $subject, Layout $result, ResponseInterface $httpResponse): Layout
     {
+        if ($this->config->isEnabled() === false) {
+            return $result;
+        }
+        
         if ($httpResponse instanceof Http) {
             $content = (string)$httpResponse->getContent();
             $httpResponse->setContent($this->convertor->execute($content));
